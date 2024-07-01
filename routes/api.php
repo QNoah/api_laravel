d29b667a905cc03350c754e47e4b65106a8e844c
Quinten <quinten.nagel01@gmail.com>
2024-07-01 11:43:42 +0200

    middleware added for restriction get or post

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 
+Route::middleware(['check.header'])->group(function (){
 Route::get('/messages', [ContactController::class, 'index']);
 Route::post('/messages', [ContactController::class, 'store']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
